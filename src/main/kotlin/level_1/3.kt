870936900ba5a02891d3c187f7c9f8416877d253
EgorDevRus <egor.rus.edu@gmail.com>
2025-12-01 23:17:14 +0300

    KB-1-3

---
+++ src/main/kotlin/level_1/3.kt
@@ -0,0 +1,15 @@
+package org.example.level_1
+
+fun main(){
+    val year: Int = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println("год взлета: $year")
+    println("час взлета: $hour")
+    println("минута взлета: $minute")
+
+    hour = 10
+    minute = 55
+    println("время посадки: $hour:$minute")
+}
